76366658e102242e1e8a10596015ada25958221f
rinornor <rinornor@gmail.com>
2023-03-29 22:46:11 +0200

    Added some markup messages

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
+// 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
